8620a4c4890dfa8d61e0b1c1fdd312e05fda163b
Agust Rafnsson <agust@urbit.com>
2020-10-06 13:46:42 +0200

    initial commit

---
+++ GCDWebServerDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GCDWebServerDemo
+//
+//  Created by Agust Rafnsson on 2020-10-06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,6 @@
+target 'GCDWebServerDemo' do
+  pod "GCDWebServer", "~> 3.0"
+  pod "GCDWebServer/WebUploader", "~> 3.0"
+end
+
+
